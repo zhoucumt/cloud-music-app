05fbf51352f287b2e14d3db772e87da2ebb5485a
zhoucumt <553817478@qq.com>
2021-09-23 17:10:35 +0800

    从数据库获取歌单信息并实现触底更新

---
+++ cloudfunctions/music/index.js
@@ -0,0 +1,17 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk');
+
+cloud.init({
+  env: cloud.DYNAMIC_CURRENT_ENV,
+});
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  return await cloud.database().collection('playlist')
+  .skip(event.start)
+  .limit(event.count)
+  .orderBy('createTime', 'desc')
+  .get().then(res => {
+    return res;
+  })
+}
