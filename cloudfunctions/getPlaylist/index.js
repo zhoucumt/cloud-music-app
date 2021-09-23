47bcb540db143110ce00e7893e8c61f394afd883
zhoucumt <553817478@qq.com>
2021-09-23 15:05:33 +0800

    歌单去重后再插入数据库

--- cloudfunctions/getPlaylist/index.js
+++ cloudfunctions/getPlaylist/index.js
@@ -9,18 +9,34 @@ const db = cloud.database();
 const rp = require('request-promise');
 // const URL = 'http://musicapi.xiecheng.live/personalized';
 const URL = 'https://autumnfish.cn/personalized';
-// const playlistCollection = db.collection('playlist')
+const playlistCollection = db.collection('playlist')
 
 // 云函数入口函数
 exports.main = async (event, context) => {
+  const list = await playlistCollection.get();
+
   const playlist = await rp(URL).then((res) => {
     return JSON.parse(res).result;
   });
 
-  for (let i = 0, len = playlist.length; i < len; i++) {
-    await db.collection('playlist').add({
+  const newData = [];
+  // 去重
+  for (let i = 0, len1 = playlist.length; i < len1; i++) {
+    let flag = true;
+    for (let j = 0, len2 = list.data.length; j < len2; j++) {
+      if (playlist[i].id === list.data[j].id) {
+        flag = false;
+        break;
+      }
+    }
+    if (flag) {
+      newData.push(playlist[i])
+    }
+  }
+  for (let i = 0, len = newData.length; i < len; i++) {
+    await playlistCollection.add({
       data: {
-        ...playlist[i],
+        ...newData[i],
         createTime: db.serverDate(),
       }
     }).then(res => {
@@ -29,4 +45,6 @@ exports.main = async (event, context) => {
       console.log('失败');
     });
   }
+
+  return newData.length;
 }
