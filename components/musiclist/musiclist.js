0ecfb55aa7216266790eb0995328f916269d78e9
zhoucumt <553817478@qq.com>
2021-09-23 23:11:51 +0800

    歌曲列表点击后跳转到新增的播放页面

--- components/musiclist/musiclist.js
+++ components/musiclist/musiclist.js
@@ -18,6 +18,17 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+    onSelect(event) {
+      // 事件源 事件处理函数 事件对象 事件类型
+      // console.log(event.currentTarget.dataset.musicid)
+      const ds = event.currentTarget.dataset;
+      const musicid = ds.musicid;
+      this.setData({
+        playingId: musicid
+      });
+      wx.navigateTo({
+        url: `../../pages/player/player?musicId=${musicid}&index=${ds.index}`,
+      });
+    }
   }
 })
