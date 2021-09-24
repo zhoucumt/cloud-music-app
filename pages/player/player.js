379ed915354afb366b2192507dfdfbdeebff1fb8
zhoucumt <553817478@qq.com>
2021-09-24 20:34:41 +0800

    播放器页面实现背景图片和标题显示歌曲名称

--- pages/player/player.js
+++ pages/player/player.js
@@ -1,18 +1,41 @@
 // pages/player/player.js
+let musiclist = [];
+// 正在播放歌曲的index
+let nowPlayingIndex = 0;
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    // 封面图片
+    picUrl: '',
+    isPlaying: false, // false表示不播放，true表示正在播放
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    console.log(options);
+    nowPlayingIndex = options.index;
+    musiclist = wx.getStorageSync('musiclist');
+    this._loadMusicDetail(options.musicId);
+  },
 
+  // 根据id获取歌曲详情
+  _loadMusicDetail(musicId) {
+    let music = musiclist[nowPlayingIndex];
+    // 标题显示为歌曲的名称
+    wx.setNavigationBarTitle({
+      title: music.name,
+    });
+
+    this.setData({
+      picUrl: music.al.picUrl,
+      isPlaying: false,
+    });
   },
 
   /**
