6a0749e2b5d06d3c09770ace1bb953d8d7ac1377
zhoucumt <553817478@qq.com>
2021-09-25 17:18:21 +0800

    获取和设置歌曲总时间并显示在进度条中

--- components/progress-bar/progress-bar.js
+++ components/progress-bar/progress-bar.js
@@ -1,4 +1,9 @@
 // components/progress-bar/progress-bar.js
+let movableAreaWidth = 0;
+let movableViewWidth = 0;
+const backgroundAudioManager = wx.getBackgroundAudioManager();
+let duration = 0 // 当前歌曲的总时长，以秒为单位
+
 Component({
   /**
    * 组件的属性列表
@@ -22,6 +27,7 @@ Component({
   lifetimes: {
     ready() {
       this._getMovableDis();
+      this._bindBGMEvent();
     },
   },
 
@@ -29,13 +35,86 @@ Component({
    * 组件的方法列表
    */
   methods: {
+    // 获取可移动区域相关的宽度
     _getMovableDis() {
       const query = this.createSelectorQuery();
       query.select('.movable-area').boundingClientRect();
       query.select('.movable-view').boundingClientRect();
       query.exec((rect) => {
         console.log(rect);
-      })
+        // 设置可移动区域的宽
+        movableAreaWidth = rect[0].width;
+        // 设置圆圈的宽
+        movableViewWidth = rect[1].width;
+      });
+    },
+
+    // 播放相关的事件监听
+    _bindBGMEvent() {
+      backgroundAudioManager.onPlay(() => {
+      });
+
+      backgroundAudioManager.onStop(() => {
+        console.log('onStop');
+      });
+
+      backgroundAudioManager.onPause(() => {
+        console.log('Pause');
+      });
+
+      backgroundAudioManager.onWaiting(() => {
+        console.log('onWaiting')
+      });
+
+      backgroundAudioManager.onCanplay(() => {
+        console.log('onCanplay');
+        // console.log(backgroundAudioManager.duration)
+        if (typeof backgroundAudioManager.duration != 'undefined') {
+          this._setTime();
+        } else {
+          // 防止有的机型上的坑
+          setTimeout(() => {
+            this._setTime();
+          }, 1000)
+        }
+      });
+
+      backgroundAudioManager.onEnded(() => {
+        console.log("onEnded");
+      });
+
+      backgroundAudioManager.onError((res) => {
+        console.error(res.errMsg);
+        console.error(res.errCode);
+        wx.showToast({
+          title: '错误:' + res.errCode,
+        })
+      });
+    },
+
+    // 设置歌曲总时间
+    _setTime() {
+      duration = backgroundAudioManager.duration;
+      const durationFmt = this._dateFormat(duration);
+      this.setData({
+        ['showTime.totalTime']: `${durationFmt.min}:${durationFmt.sec}`
+      });
+    },
+
+    // 格式化时间
+    _dateFormat(sec) {
+      // 分钟
+      const min = Math.floor(sec / 60);
+      sec = Math.floor(sec % 60);
+      return {
+        'min': this._parse0(min),
+        'sec': this._parse0(sec),
+      };
+    },
+
+    // 补零
+    _parse0(sec) {
+      return sec < 10 ? '0' + sec : sec;
     },
   }
 })
